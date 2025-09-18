35ac644da6a320aff0828e8253b8acdec0111233
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-18 18:53:37 +0500

    Nested Loop

---
+++ Loops in cpp/Nested_Loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int week = 1; week <= 3; week++)
+    {
+        for  (int day = 1; day <= 7; day++)
+        {
+            cout << "Week: " << week << "   Day: " << day << endl;
+        }
+        cout << endl;
+    }
+}
