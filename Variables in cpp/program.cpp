229975b6d1d045b0a54ac9a0c4185e99575bf391
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-18 06:38:06 +0500

    Intro to Variables

---
+++ Variables in cpp/program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;      // variable declaration
+    a = 10;     // variable initialization
+    cout << "Your number is: " << a;
+}
+
+// = is the Assignment Operator
