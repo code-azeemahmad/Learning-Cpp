e0abfe0635cbb104738d20ebf5b72a6e0ba9edd7
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-18 06:17:14 +0500

    Taking Output from user

---
+++ Taking input and output/output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!";
+}
+
+
+// Insertion Operator
+
+/*
+main() {
+    brace on same line --> K&R style
+}
+
+main() 
+{
+    brace on next line --> Allman style
+}
+*/
