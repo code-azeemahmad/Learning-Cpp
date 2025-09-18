1ba8139b1bd6acfe5c521f0a8065bbf926183386
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-18 06:20:30 +0500

    Taking input from User

---
+++ Taking input and output/Input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int a, b;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b;
+    cout << a << endl;
+    cout << b;
+}
+
+// >> is Stream Extraction Operator
