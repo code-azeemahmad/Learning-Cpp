14d8ffda1b0272bd5a051c028ad6b4bb404eac1e
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-18 18:02:44 +0500

    While loop

---
+++ Loops in cpp/While_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    while (count != -1)
+    {   
+        cout << "Enter a number: ";
+        cin >> count;
+        cout << "I am in UET!" << endl;
+    }
+}
