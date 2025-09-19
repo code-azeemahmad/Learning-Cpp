32a1f893d2b80e888465aa6a9522147ee7b5a0a2
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-19 09:01:17 +0500

    Pass by Reference

---
+++ Variables in cpp/pass_by_reference.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void swap(int &n1, int &n2);
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter First Number: ";
+    cin >> num1;
+    cout << "Enter Second Number: ";
+    cin >> num2;
+    cout << num1 << "\t" << num2 << endl;
+    swap(num1, num2);
+    cout << num1 << "\t" << num2 << endl;
+}
+
+void swap(int &n1, int &n2)
+{
+    int temp = n2;
+    n2 = n1;
+    n1 = temp;
+}
