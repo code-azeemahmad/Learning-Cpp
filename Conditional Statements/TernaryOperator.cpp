018c22ee5fc6f76139d28ff054604dc2ab32be04
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-20 07:12:22 +0500

    Ternary Operator in cpp

---
+++ Conditional Statements/TernaryOperator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = -45;
+    cout << (num >=  0 ? "Postive": "Negative") <<endl;
+
+    return 0;
+}
+
+// (condition ? statement1 : statement2)
