bf205cace64bd76a15a9794e0970cdf96fffe119
code-azeemahmad <azeemahmadd9@gmail.com>
2025-09-20 06:47:41 +0500

    Operators in cpp

---
+++ Operators in cpp/operator.cpp
@@ -0,0 +1,28 @@
+/*
+Operators in C++
+
+--> Arithmetic
+--> Relational
+--> Logical
+
+ */
+
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+    int a = 5;
+    int b = 2;
+    int c = a / b;
+    cout << c << endl;
+
+    int _a = 5;
+    float _b = 2;
+    float _c = _a / _b;
+    cout << _c;
+    return 0;
+ }
+
+ //     int / int --> int
+ //     int / float --> float
